01a99de3636ff797f61757b687d24d6dce955d8d
AlbertSolomon <solomonaalbert@gmail.com>
2023-08-29 16:16:17 +0200

    we keep freeeeeing

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_listint2 - free the memory of the list
+ * @head: pointer to the provided list
+ *
+ * Return: returns void
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *curr = *head;
+
+	while (curr != NULL)
+	{
+		listint_t *temp = curr;
+
+		curr = curr->next;
+		free(temp);
+	}
+	*head = NULL;
+}
