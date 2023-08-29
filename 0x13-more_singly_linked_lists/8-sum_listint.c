f1935cfcfc300b1e51bb9f5c4a4a10f775ac767f
AlbertSolomon <solomonaalbert@gmail.com>
2023-08-29 10:35:19 +0200

    sum of of a singly linked list

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "list.h"
+#include <stdlib.h>
+
+/**
+ * sum_listint - calculates the sum of a singly linked list
+ * @head:  pointer to the linked list provided
+ *
+ * Return: The sum of elements in a linked list
+ */
+int sum_listint(listint_t *head)
+{
+	if (head == NULL)
+		return (0);
+
+	int sum = 0;
+	listint_t *tmp = head;
+
+	while (tmp != NULL)
+	{
+		sum += tmp->n;
+		tmp = tmp->next;
+	}
+	return (sum);
+}
