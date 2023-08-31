793cfa2abaac1f3b89bc58f7e5cc4f572bf2c2f1
AlbertSolomon <solomonaalbert@gmail.com>
2023-09-01 00:11:56 +0200

    first to go

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,37 +1,33 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
- * binary_to_uint - function that converts numbers from binary to unsigned
- * @b: pointer pointing to the location of the provided prompts
- *
- * Return: returns the converted numbers of type unsigned int
+ * binary_to_uint- function that converts binary to unsigned int
+ * @b: pointer to the provided prompt, to be converted
+ * Return: converted number of unsigned int
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int res = 0, one, i;
-	int l = 0;
 
-	if (b == 0)
-		return (0);
+	int val = 0, j;
+	unsigned int number = 0, non_binary = 0;
 
+	if (b == NULL)
+		return (non_binary);
 
-	while (b[l] != 0)
+	while (b[val] != '\0')
+		val++;
+	val -= 1;
+	j = 0;
+	while (b[j])
 	{
-		if (b[l] != '0' && b[l] != '1')
-			return (0);
-		l++;
-	}
+		if ((b[j] != '0') && (b[j] != '1'))
+			return (non_binary);
 
-	l = l - 1;
+		if (b[j] == '1')
+			number += (1 * (1 << val));
+		j++;
 
-	for (i = 0; l >= 0; i++, l--)
-	{
-		if (b[l] == '0')
-			one = 0;
-		else if (b[l] == '1')
-			one = 1;
-		one = one << i;
-		res = res | one;
+		val--;
 	}
-	return (res);
+	return (number);
 }
